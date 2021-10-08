0553932c2e8b671e7fbcb86a5aa8df67f61c88b7
Harshit Negi <negiharshit0@gmail.com>
2021-10-08 13:44:12 +0530

    updated label

--- src/components/Route.js
+++ src/components/Route.js
@@ -1,4 +1,4 @@
-import { React, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 const Route = ({ path, children }) => {
   const [currentURL, setCurrentURL] = useState(window.location.pathname);
